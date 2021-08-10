278e72d941921faecb4322a7006afc5b76c6d4df
Annabelle Rutters <annabelle4086@gmail.com>
2021-08-10 14:45:41 -0400

    Exercise 35.py

--- ex34.py
+++
@@ -1,8 +0,0 @@
-animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'playpus']
-bear = animals[0]
-
-
-
-# [0] is a cardinal number aka index1. another example of this is [1] which is index 2 or the second in a list
-# ordinal == ordered, 1st
-# cardinal === cards at random, 0
