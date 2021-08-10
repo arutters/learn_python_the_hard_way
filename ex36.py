397ba6f13ac671ff3920e49fc908605b5c11753d
Annabelle Rutters <annabelle4086@gmail.com>
2021-08-10 15:13:01 -0400

    Exercise 36 Notes

---
+++ ex36.py
@@ -0,0 +1,17 @@
+# Rules for if-statements
+# every if statement must have an else:
+# if this else shouldnt run becuase it doesnt make sense, a die function must be included. Itll just print an error message(will find many)
+# never test if statments more than two deep and always try to do them one
+# treat if statemtns like paragraphs where each if-elif-else grouping is like a set of sentences. put blank lines before and after
+# your Boolean test should be simple, if not move their calculations to earlier variables in your function and use a good name for the variable
+
+
+#Rules for loops
+# Use a while-loop only to loop forever(which means never). Only applies to Python
+# Use a for-loop for all other kinds of looping, especially is there is a fixed or limited number of things to loop over
+
+# Tips for Debugging
+# Do NOT use a "debugger" its the equivalent of a full body scan on a sick person. You dont get any specific useful info, and you
+# find a lot of info that doesnt help and is just confusing
+# The best way to debug is to use print to print out the values of variables in points of the program to see where they go wrong
+# Make sure parts of your programs work as you work on them. Dont write massive files of code before you try to run them. 
